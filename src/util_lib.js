c232a66e077ef592c5f407665d2d0fa7b666593e
bsrushti <bsrushti@in-bsrushti.local>
2018-10-27 19:09:20 +0530

    Added generalized function for making object in util_lib.js
    Exporting each functions in new line.

--- src/util_lib.js
+++ src/util_lib.js
@@ -1,3 +1,23 @@
+const createObject = function(inputArray) {
+  return {
+    type : inputArray[2],
+    height : inputArray[3],
+    width : inputArray[4],
+  }
+}
+
+const createDiamondObject = function(inputArray) {
+  return createObject(inputArray); 
+}
+
+const createTriangleObject = function(inputArray) {
+  return createObject(inputArray); 
+} 
+
+const createRectangleObject = function(inputArray) {
+  return createObject(inputArray); 
+} 
+
 const repeatCharacters = function(character, numberOfTimes) {
   let line = "";
   for(let index = 0; index < numberOfTimes; index++) {
@@ -18,5 +38,13 @@ const repeatSpaces = function(numberOfTimes) {
   return repeatCharacters(" ",numberOfTimes);
 }
 
-module.exports={repeatCharacters,repeatStars,repeatSpaces,repeatHyphen};
+module.exports={
+  repeatCharacters,
+  repeatStars,
+  repeatSpaces,
+  repeatHyphen,
+  createDiamondObject,
+  createTriangleObject,
+  createRectangleObject
+};
 
